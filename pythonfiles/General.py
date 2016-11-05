962a1634fdae028d86c664f60bed7a83b99a8757
rycao18 <rycao18@gmail.com>
2016-11-05 21:47:57 +0000

    WORKING FACE COMMIT

--- pythonfiles/General.py
+++ pythonfiles/General.py
@@ -13,8 +13,8 @@ def getFace(imgPath, cPath):
     # Detect faces in the image
     return faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
     
-faces = getFace("./grouppicture.jpg", "./classifier.xml")
-image = open("./grouppicture.jpg")
+faces = getFace("./picture.jpg", "./classifier.xml")
+image = open("./picture.jpg")
 #for (x, y, w, h) in faces:
 #    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 #cv2.imshow("Faces found" ,image)
