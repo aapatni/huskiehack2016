6738314a13b0cbfd784eb650977251cc17a93199
rycao18 <rycao18@gmail.com>
2016-11-05 20:59:27 +0000

    Initial commit

---
+++ pythonfiles/General.py
@@ -0,0 +1,22 @@
+import cv2
+import sys
+
+def getFace(imgPath, cPath):
+    imagePath = imgPath
+    cascPath = cPath
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    # Read the image
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    return faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+    
+faces = getFace(sys.argv[0], sys.argv[1])
+print "Found {0} faces!".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Faces found" ,image)
+cv2.waitKey(0)
