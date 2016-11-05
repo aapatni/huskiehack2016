2ce942d692731faea6cedbd7b608d533cc6077d7
rycao18 <rycao18@gmail.com>
2016-11-05 21:49:55 +0000

    TOTAL COMMIT FACE

--- pythonfiles/General.py
+++ pythonfiles/General.py
@@ -20,3 +20,16 @@ image = open("./picture.jpg")
 #cv2.imshow("Faces found" ,image)
 #cv2.waitKey(0)
 print faces
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+         roi_gray = gray[y:y+h, x:x+w]
+         roi_color = img[y:y+h, x:x+w]
+         eyes = eye_cascade.detectMultiScale(roi_gray)
+         for (ex,ey,ew,eh) in eyes:
+    8         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    9 
+   10 cv2.imshow('img',img)
+   11 cv2.waitKey(0)
+   12 cv2.destroyAllWindows()
